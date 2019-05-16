4be9f5cb994e00a13de3892e2ee8d4be1b081312
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2019-05-16 15:54:45 +0000

    added back format

--- src/build.js
+++ src/build.js
@@ -4,38 +4,31 @@ var yaml = require('yaml');
 
 var TEMPLATES = path.join( process.cwd(), 'src', 'templates' );
 
-// var allFiles = dir => {
-//     var files = [];
-//     fs.readdirSync( dir ).forEach( filename => {
-//         var file = path.join( dir, filename );
-//         if ( fs.statSync( file ).isDirectory() ) {
-//             files.push( ...allFiles( file ) );
-//         } else {
-//             files.push( file );
-//         }
-//     })
-//     return files;
-// }
-
-var allFiles = dir => fs.readdirSync( dir )
-    .map( filename => path.join( dir, filename ) )
-    .reduce( ( files, file ) => 
-        fs.statSync( file ).isDirectory()
-            ? [ ...files, ...allFiles( file ) ]
-            : [ ...files, file ]
-    , [] )
+var allFiles = dir => {
+    var files = [];
+    fs.readdirSync( dir ).forEach( filename => {
+        var file = path.join( dir, filename );
+        if ( fs.statSync( file ).isDirectory() ) {
+            files.push( ...allFiles( file ) );
+        } else {
+            files.push( file );
+        }
+    })
+    return files;
+}
 
 var renderSite = ( inDir, outDir ) => {
     allFiles( inDir ).forEach( file => {
         var url = path.relative( inDir, file );
+        console.log( `Rendering ${ url }` );
         var content = yaml.parse( fs.readFileSync( file, 'utf8' ) ) || {};
         if ( !content.template ) {
-            console.log( `No template field in ${ file }` );
+            console.error( `No template field` );
             return;
         }
         var templateFile = path.join( TEMPLATES, content.template + '.js' );
         if ( !fs.existsSync( templateFile ) ) {
-            console.log( `No template field in ${ file }` );
+            console.error( `Template ${ templateFile } not found` );
             return;
         }
         var template = require( templateFile );
@@ -44,32 +37,6 @@ var renderSite = ( inDir, outDir ) => {
         var outFile = path.join( outDir, outFilename );
         fs.writeFileSync( outFile, html, 'utf8' );
     })
-    // var renderDirectory = dir => {
-    //     fs.readdirSync( dir ).forEach( filename => {
-    //         var file = path.join( dir, filename );
-    //         if ( fs.statSync( file ).isDirectory() ) {
-    //             renderDirectory( file );
-    //         } else {
-    //             var url = path.relative( inDir, file );
-    //             var content = yaml.parse( fs.readFileSync( file, 'utf8' ) ) || {};
-    //             if ( !content.template ) {
-    //                 console.log( `No template field in ${ file }` );
-    //                 return;
-    //             }
-    //             var templateFile = path.join( TEMPLATES, content.template + '.js' );
-    //             if ( !fs.existsSync( templateFile ) ) {
-    //                 console.log( `No template field in ${ file }` );
-    //                 return;
-    //             }
-    //             var template = require( templateFile );
-    //             var html = template( content );
-    //             var outFilename = path.basename( url ) + '.html'
-    //             var outFile = path.join( outDir, outFilename );
-    //             fs.writeFileSync( outFile, html, 'utf8' );
-    //         }
-    //     })
-    // }
-    // renderDirectory( inDir );
 }
 
 renderSite( './src/content', './dist' );
