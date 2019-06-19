21d60062882e8c069d049dc8e71c136d2e200e9c
Ubuntu <ubuntu@ip-172-31-20-91.us-east-2.compute.internal>
2019-06-19 14:11:15 +0000

    changed markdown config

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,7 +1,9 @@
 var html = require('nanohtml');
-
+var raw = require('nanohtml/raw');
 var layout = require('./partials/layout');
+var marked = require('marked');
 
 module.exports = ( page, site ) => layout( html`
     ${ page.title }
+    ${ raw( marked( page.body || '' ) ) }
 `)
